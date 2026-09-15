66c7f13c57abe9287666d08de826c07213efcae3
vaishnavikagale-lab <vaishnavikagale@gmail.com>
2026-09-15 09:59:17 +0530

    password

--- password.py
+++ password.py
@@ -1,7 +1,9 @@
 corerct_pass = "Correct_pass"
 not_found = True
+
 while not_found:
     password = input("Enter password : ")
     if password == corerct_pass:
         not_found == False
+
         print("Password matched succesfully!!")
